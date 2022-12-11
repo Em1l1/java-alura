55d18a43be3779c134d73d3c11a9251996c21420
Emily <blancocronos@hotmail.com>
2022-12-10 22:55:47 -0600

    multiplicacion

---
+++ java-first/loops/EjemploCiclos.java
@@ -0,0 +1,16 @@
+public class EjemploCiclos {
+
+  public static void main(String[] args) {
+
+    int contador = 0;
+
+    while(contador < 10) { // mientras que (condicion)
+      // Ejecuta esto
+      System.out.println(contador);
+      // contador += 1;
+      // contador = contador + 1;
+      contador ++;
+    }
+  }
+  
+}
---
+++ java-first/loops/EjemploCiclosFor.java
@@ -0,0 +1,18 @@
+public class EjemploCiclosFor {
+
+  public static void main(String[] args) {
+
+
+    for (int contador = 0; contador <= 10; contador++) {
+      System.out.println(contador);
+    }
+    System.out.println(contador);
+  }
+
+  for (int count = 0; count <= 10; count++) {
+    System.out.println(count);
+  }
+
+
+  
+}
