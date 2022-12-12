c5b7250e5365474fc42b9715a9ef4fc36f690181
Emily <blancocronos@hotmail.com>
2022-12-12 15:37:07 -0600

    informacion por defecto

--- java-OO/CrearCuenta.java
+++ java-OO/CrearCuenta.java
@@ -9,5 +9,7 @@ public class CrearCuenta {
         Cuenta segundaCuenta = new Cuenta();
         segundaCuenta.saldo = 500;
         System.out.println(segundaCuenta.saldo);
+        System.out.println(primeraCuenta.saldo);
+        System.out.println(primeraCuenta.agencia);
     }
 }
