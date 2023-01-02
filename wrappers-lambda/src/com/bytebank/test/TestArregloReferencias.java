3d4f4bb9b01cc694650189c9e48a072abe1aa56f
victoriano <mytacam78@gmail.com>
2023-01-02 16:55:55 -0600

    Cast Object 2

--- wrappers-lambda/src/com/bytebank/test/TestArregloReferencias.java
+++ wrappers-lambda/src/com/bytebank/test/TestArregloReferencias.java
@@ -1,34 +1,42 @@
 package com.bytebank.test;
 
-import com.bytebank.modelo.Cuenta;
-import com.bytebank.modelo.CuentaAhorros;
-import com.bytebank.modelo.CuentaCorriente;
+import com.bytebank.modelo.*;
+//import com.bytebank.modelo.Cuenta;
+//import com.bytebank.modelo.CuentaAhorros;
+//import com.bytebank.modelo.CuentaCorriente;
 
 public class TestArregloReferencias {
 
     public static void main(String[] args) {
 
-        CuentaCorriente cc = new CuentaCorriente(23, 44);
+
+    	Object[] referencias = new Object[5];
 
         //                          [ new |cc |null  |null  |null  ]
-        Cuenta[] cuentas = new Cuenta[5];
-        cuentas[1] = cc;
+        CuentaCorriente cc = new CuentaCorriente(23, 44);
+        referencias[1] = cc;
+        
+        Cliente cliente = new Cliente();
+        referencias[4] = cliente;
+        
+        Cliente obtenido = (Cliente) referencias[4];
+        System.out.println(obtenido);
 
         CuentaAhorros ca = new CuentaAhorros(44, 55);
-		cuentas[3] = ca;
+		referencias[3] = ca;
 
         System.out.println(cc);
-        System.out.println(cuentas[1]);
+        System.out.println(referencias[1]);
 
-        cuentas[0] = new CuentaCorriente(11, 99);
-        System.out.println(cuentas[0]);
+        referencias[0] = new CuentaCorriente(11, 99);
+        System.out.println(referencias[0]);
         
         // @Cast
-        CuentaCorriente cuenta = (CuentaCorriente) cuentas[1];
+        CuentaCorriente cuenta = (CuentaCorriente) referencias[1];
         System.out.println(cuenta);
 
-        for (int i = 0; i < cuentas.length; i++) {
-            System.out.println(cuentas[i]);
+        for (int i = 0; i < referencias.length; i++) {
+            System.out.println(referencias[i]);
         }
     }
 
