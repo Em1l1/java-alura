b5c675af43342f5fc0707cc92d32d2a32fa37a6f
victoriano <mytacam78@gmail.com>
2023-01-05 23:24:39 -0600

    integer

---
+++ wrappers-lambda/src/com/bytebank/test/TestRepasoArray.java
@@ -0,0 +1,20 @@
+package com.bytebank.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestRepasoArray {
+
+	public static void main(String[] args) {
+		int[] numeros = new int[10];
+		
+		int numero = 40;
+		Integer numeroObjeto = new Interger(40);
+		
+		List<Integer> lista = new ArrayList<Integer>();
+		primitivo != object
+		lista.add(numero);      // autoboxing
+    lista.add(new Integer(40));
+		lista.add(numeroObjeto);
+	}
+}
