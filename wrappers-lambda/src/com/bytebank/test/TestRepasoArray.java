1b8339307e8977e0e06a97e2e03ec83601dd467a
victoriano <mytacam78@gmail.com>
2023-01-05 23:33:33 -0600

    autoboxing

--- wrappers-lambda/src/com/bytebank/test/TestRepasoArray.java
+++ wrappers-lambda/src/com/bytebank/test/TestRepasoArray.java
@@ -9,12 +9,29 @@ public class TestRepasoArray {
 		int[] numeros = new int[10];
 		
 		int numero = 40;
-		Integer numeroObjeto = new Interger(40);
+		// Integer numeroObjeto = new Interger(40);     // deprecado
+    Integer numeroObjeto = Integer.valueOf(40);
 		
 		List<Integer> lista = new ArrayList<Integer>();
 		primitivo != object
 		lista.add(numero);      // autoboxing
-    lista.add(new Integer(40));
+    // lista.add(new Integer(40));
+    lista.add(Integer.valueOf(40));
 		lista.add(numeroObjeto);
+
+
+    // unboxing
+    // int valorPrimitivo = numeroObjeto;
+    int valorPrimitivo = numeroObjeto.intValue();
+
+    byte byteInteger = numeroObjeto.byteValue.byteValue();
+    double doubleInteger = numeroObjeto.doubleObjeto.doubleValue();
+    float floatIOnteger = numeroObjeto.floatValue();
+
+    System.out.println(Integer.MIN_VALUE);
+    System.out.println(Integer.MAX_VALUE);
+
+    System.out.println(Integer.BYTES);
+    System.out.println(Integer.SIZE);
 	}
 }
