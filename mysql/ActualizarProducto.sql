9835fc544df53654a62a3799bbacdd3866c3894c
victoriano <mytacam78@gmail.com>
2023-01-19 08:54:05 -0600

    update producto

---
+++ mysql/ActualizarProducto.sql
@@ -0,0 +1,5 @@
+UPDATE tbproductos SET sabor = "Asái", precio = 50.30
+WHERE producto = "695594";
+
+UPDATE tbproductos SET sabor = "Mango", precio = 99.90
+WHERE producto = '1041119';
---
+++ mysql/insertarProducto.sql
@@ -0,0 +1,1 @@
+INSERT INTO tbproductos (PRODUCTO, NOMBRE, VOLUMEN, SABOR, ENVASE, PRECIO) VALUES ('773912', 'Clean', '1 Litro', 'Naranja', 'Botella PET', 8.01);
