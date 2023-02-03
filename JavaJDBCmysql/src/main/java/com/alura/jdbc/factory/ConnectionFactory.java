8fc5159570bba9de900fed229d1ff9afe4b3d3d9
Emily <blancocronos@hotmail.com>
2023-02-02 18:44:27 -0600

    creado un pool de conexiones

--- JavaJDBCmysql/src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
+++ JavaJDBCmysql/src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
@@ -4,6 +4,9 @@
  */
 package com.alura.jdbc.factory;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -13,10 +16,21 @@ import java.sql.SQLException;
  * @author robot
  */
 public class ConnectionFactory {
+  private DataSource datasource;
+  // ?pool de conexiones
+  public ConnectionFactory() {
+    var pooledDataSource = new ComboPooledDataSource();
+    pooledDataSource.setJdbcUrl("jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC");
+    pooledDataSource.setUser("root");
+    pooledDataSource.setPassword("12345");
+
+    this.datasource = pooledDataSource;
+  }
 
   public Connection recuperaConexion() throws SQLException {
     // TODO Connecion dataBase
-    String password = "12345";
-    return DriverManager.getConnection("jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC", "root", password);
+//    String password = "12345";
+//    return DriverManager.getConnection("jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC", "root", password);
+      return this.datasource.getConnection();
    }
 }
