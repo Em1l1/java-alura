5ed5d996a849594bafe69ffc0d1a39e3af239515
Emily <blancocronos@hotmail.com>
2023-03-16 19:21:42 -0600

    filtrar datos con like

---
+++ mysql_advance/Learn/4_filtar-like.sql
@@ -0,0 +1,8 @@
+USE jugos_ventas;
+
+-- SELECT * FROM tb WHERE CAMPO LIKE '%<condicion>%';
+SELECT * FROM tabla_de_productos WHERE SABOR LIKE '%manzana';
+
+SELECT * FROM tabla_de_productos WHERE SABOR LIKE '%manzana%' AND ENVASE = 'Botella PET';
+
+SELECT * FROM tabla_de_clientes WHERE NOMBRE LIKE '%ez';
