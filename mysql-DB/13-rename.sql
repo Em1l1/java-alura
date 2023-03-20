7d47acf302b464183785104bc00323281109b9b3
victoriano <mytacam78@gmail.com>
2023-03-20 01:27:51 -0600

    rename table

---
+++ mysql-DB/13-rename.sql
@@ -0,0 +1,11 @@
+USE ventas_jugos;
+
+ALTER TABLE tb_venta RENAME tb_factura;
+
+ALTER TABLE facturas RENAME tb_factura;
+
+-- ALTER TABLE cliente RENAME tb_cliente;
+
+ALTER TABLE items_facturas RENAME tb_items_facturas;
+
+SHOW TABLES;
