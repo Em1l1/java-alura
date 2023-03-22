5fd4d42dff0c57d6c52ddca17fe6623f308c0115
victoriano <mytacam78@gmail.com>
2023-03-22 16:07:30 -0600

    doc: trigger

--- mysql-DB/26-commit-rollback.sql
+++ mysql-DB/26-commit-rollback.sql
@@ -12,6 +12,54 @@ INSERT INTO ventas_jugos . tb_vendedor
   'Fernando Ruiz',
   'Oblatos',
   0.1,
-  '2016--6-14',
+  '2016-6-14',
   0
 );
+
+SELECT * FROM tb_vendedor;
+
+START TRANSACTION;
+
+INSERT INTO ventas_jugos . tb_vendedor
+(
+  MATRICULA,
+  NOMBRE,
+  BARRIO,
+  COMISION,
+  FECHA_ADMISION,
+  DE_VACACIONES
+) VALUES  
+(
+  '257',
+  'Fernando Rojas',
+  'Oblatos',
+  0.1,
+  '2016-6-14',
+  0
+);
+
+
+
+INSERT INTO ventas_jugos . tb_vendedor
+(
+  MATRICULA,
+  NOMBRE,
+  BARRIO,
+  COMISION,
+  FECHA_ADMISION,
+  DE_VACACIONES
+) VALUES  
+(
+  '257',
+  'David Rojas',
+  'Del Valle',
+  0.1,
+  '2016-6-14',
+  0
+);
+
+UPDATE tb_vendedor SET COMISION = COMISION * 1.05; 
+
+ROLLBACK;
+
+COMMIT;
