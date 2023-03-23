dec7f6dddda355a4c58b43f7c683622325ad66cd
Emily <blancocronos@hotmail.com>
2023-03-23 08:24:37 -0600

    🔍 chore(joins.sql): add join queries for facturas and items_facturas tables Added two queries to join the facturas and items_facturas tables to retrieve the total revenue for each year. The first query uses an INNER JOIN to join the two tables, while the second query uses a LEFT JOIN to include all the rows from the facturas table and only matching rows from the items_facturas table.
    🔍 chore(left-right.sql): add left and right join queries for tabla_de_clientes and facturas tables
    Added four queries to join the tabla_de_clientes and facturas tables using LEFT and RIGHT JOINs. The first query uses an INNER JOIN to join the two tables, while the second query uses a LEFT JOIN to include all the rows from the tabla_de_clientes table and only matching rows from the facturas table. The third query uses a LEFT JOIN to include all the rows from the tabla_de_clientes table that do not

---
+++ mysql_advance/Learn/4_tablas-consultas/2-left-right.sql
@@ -0,0 +1,28 @@
+USE jugos_ventas;
+
+SELECT count(*) FROM tabla_de_clientes;
+
+SELECT DISTINCT A.DNI, A.NOMBRE, B.DNI FROM tabla_de_clientes A
+INNER JOIN
+facturas B
+ON A.DNI = B.DNI;
+
+SELECT DISTINCT A.DNI, A.NOMBRE, A.CIUDAD, B.DNI FROM tabla_de_clientes A
+LEFT JOIN
+facturas B
+ON A.DNI = B.DNI;
+
+-- LEFT
+SELECT DISTINCT A.DNI, A.NOMBRE, A.CIUDAD, B.DNI FROM tabla_de_clientes A
+LEFT JOIN
+facturas B
+ON A.DNI = B.DNI
+WHERE B.DNI IS NULL;
+
+-- RIGHt
+SELECT DISTINCT B.DNI, B.NOMBRE, B.CIUDAD, A.DNI FROM facturas A
+RIGHT JOIN
+tabla_de_clientes B
+ON A.DNI = B.DNI
+WHERE A.DNI IS NULL;
+
