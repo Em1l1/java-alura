01b79556593c709ce6029c0d9b78d42ad6d6b8a4
Emily <blancocronos@hotmail.com>
2023-03-26 18:50:42 -0600

    :memo: function
    function in MySQL

---
+++ mysql_advance/Learn/5-funciones/1-funciones-str.sql
@@ -0,0 +1,16 @@
+SELECT LTRIM("                              MYSQL es facil");
+
+SELECT LTRIM("MYSQL es facil                                                  ");
+
+
+SELECT LTRIM("                                          MYSQL es facil                                                  ");
+
+SELECT CONCAT("MYSQL es facil,", " no lo cres?");
+
+SELECT upper("mysql es una base de datos interesante.");
+
+SELECT LOWER("MYSQL ES UNA BASE DE DATOS INTERESANTE.");
+
+SELECT SUBSTRING("mysql es una base de datos interesante.", 14,4);
+
+SELECT CONCAT(NOMBRE, " ", DNI) FROM tabla_de_clientes;
